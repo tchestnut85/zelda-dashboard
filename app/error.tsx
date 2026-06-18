b8a59aa86300df83cecd385ceec3a6abdbed7edd
Tom Chestnut <tchestnut85@gmail.com>
2026-06-18 16:33:10 -0400

    update error boundary

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,9 @@
 'use client';
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="flex flex-col items-center gap-5">
+      <h2 className="text-2xl">There was an error...</h2>
+    </div>
+  );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <main className="flex flex-col items-center gap-5">
-      <h2 className="text-2xl">Page Not Found...</h2>
+      <h2 className="text-2xl">404 - Page Not Found...</h2>
       <Link href="/">
         <p className="text-xl text-warriors-green bg-warriors-yellow font-bold p-3 rounded-sm">
           Back to Hyrule!
