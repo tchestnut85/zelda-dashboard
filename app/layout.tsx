d7c2a7c358239045749e1a36538daf8dc2f935d4
Tom Chestnut <tchestnut85@gmail.com>
2026-06-23 23:52:34 -0400

    update nav styling; make header and nav items mobile responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from 'next';
 import { IM_Fell_English, IM_Fell_English_SC } from 'next/font/google';
 import './globals.css';
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from '@/components/ui/navigation-menu';
 import Image from 'next/image';
 import Link from 'next/link';
-import NavItem from './components/NavItem';
+import NavMenu from './components/NavMenu';
 
 const fellEnglish = IM_Fell_English({
   variable: '--font-fell-english',
@@ -32,7 +28,7 @@ export default function RootLayout({
     <html lang="en" className={`${fellEnglish.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col bg-warriors-green">
         <header
-          className={`flex justify-around items-center py-2 mb-10 bg-twilight-green ${fellEnglishSC.variable}`}
+          className={`flex flex-wrap flex-col md:flex-row text-center justify-around items-center gap-3 lg:gap-2 py-2 mb-10 bg-twilight-green ${fellEnglishSC.variable}`}
         >
           <Link href="/">
             <Image
@@ -43,16 +39,10 @@ export default function RootLayout({
               loading="eager"
             />
           </Link>
-          <h1 className="text-warriors-yellow text-4xl">
+          <h1 className="text-warriors-yellow text-2xl md:text-3xl lg:text-4xl">
             Legend of Zelda Dashboard
           </h1>
-          <NavigationMenu className="flex items-center rounded-xs">
-            <NavigationMenuList className="flex gap-3">
-              <NavItem href="/" label="Home" />
-              <NavItem href="/heroes" label="Heroes" />
-              <NavItem href="/villains" label="Villains" />
-            </NavigationMenuList>
-          </NavigationMenu>
+          <NavMenu />
         </header>
         {children}
       </body>
